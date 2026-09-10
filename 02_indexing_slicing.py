2c38e6bf80472862620512c62fa830bff87d0fbd
habiba zainab <habibazainabali@gmail.com>
2026-09-10 11:20:00 -0700

    Set 02 Header

---
+++ 02_indexing_slicing.py
@@ -0,0 +1,9 @@
+"""
+
+===========================================================
+   LECTURE 02 - SET 02 : INDEXING & SLICING
+   Topics : Indexing, Slicing, Negative Slicing 
+   Total Questions :  
+============================================================
+
+"""
