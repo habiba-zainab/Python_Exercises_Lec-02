b5b164162dc2c054132ca625e76e0cd384c2c69c
habiba zainab <habibazainabali@gmail.com>
2026-09-10 11:30:01 -0700

    Basic indexing

--- 02_indexing_slicing.py
+++ 02_indexing_slicing.py
@@ -11,3 +11,10 @@
 # ==========================================================
 # PART A:   Indexing
 # ==========================================================
+
+# Q1: Basic indexing
+#   Given: word = "Python"
+#   Print each character using positive indexing 
+#   (0, 1, 2, 3, 4, 5)
+#   Print first and last character.
+
