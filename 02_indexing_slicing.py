fad21b1765176ba0df8945d419d304465eb13de1
habiba zainab <habibazainabali@gmail.com>
2026-09-11 09:25:39 -0700

    Total Questions

--- 02_indexing_slicing.py
+++ 02_indexing_slicing.py
@@ -3,7 +3,7 @@
 ===========================================================
    LECTURE 02 - SET 02 : INDEXING & SLICING
    Topics : Indexing, Slicing, Negative Slicing 
-   Total Questions :  
+   Total Questions :  09
 ============================================================
 
 """
