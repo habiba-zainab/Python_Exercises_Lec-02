5d8a33313f56b17503872a5dd24e0da962f4a89c
habiba zainab <habibazainabali@gmail.com>
2026-09-11 09:57:14 -0700

    Question 1

--- 03_string_functions.py
+++ 03_string_functions.py
@@ -10,3 +10,12 @@
 # ==========================================================
 # PART A:   Search Functions
 # ==========================================================
+
+# Q1: Given: text = "Python Programming is fun. Python is 
+#             powerful."
+#    Find the position of:
+#     - First occurence of "Python"
+#     - First occurence of "is"
+#     - Substring "Java" (not present)
+#     - Second occurence of "Python" 
+#       (Hint: use find with start parameter)
