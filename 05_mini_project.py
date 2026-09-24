1bd2253d54dae4462a2e3394e3815ab5260da4bc
habiba zainab <habibazainabali@gmail.com>
2026-09-24 12:12:16 -0700

    Added indexing & slicing part for email

--- 05_mini_project.py
+++ 05_mini_project.py
@@ -24,3 +24,9 @@ print("Email:", email)
 parts = email.split('@')
 username = parts[0]
 domain = parts[1]
+
+# Indexing & Slicing
+print("First letter:", email[0])
+print("Last letter:", email[-1])
+print("Username:", username)
+print("Domain:", domain[:5])  # gmail
