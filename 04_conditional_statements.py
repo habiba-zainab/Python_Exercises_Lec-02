1df39dd96bddb144e034c87e44172b128c8b4288
habiba zainab <habibazainabali@gmail.com>
2026-09-24 11:02:58 -0700

    solved program of ladder

--- 04_conditional_statements.py
+++ 04_conditional_statements.py
@@ -55,3 +55,20 @@ else:
 #     - 60-69: D
 #     - Below 60: F
 #     Print grade
+
+score = 85
+
+print("Score: ", score)
+
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade: B")
+elif score >= 70:
+    print("Grade: C")
+elif score >= 60:
+    print("Grade: D")
+else: 
+    print("Grade: F")
+
+    # ----------------------------------------------------------
