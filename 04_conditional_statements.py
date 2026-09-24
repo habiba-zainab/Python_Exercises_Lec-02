118ee5b128a1690b693a6641c01226fd3070dbe9
habiba zainab <habibazainabali@gmail.com>
2026-09-24 11:44:15 -0700

    Total Questions

--- 04_conditional_statements.py
+++ 04_conditional_statements.py
@@ -3,7 +3,7 @@
 ===========================================================
    LECTURE 02 - SET 04 : CONDITIONAL STATEMENTS
    Topics :     Conditional Statements(if, elif, else)
-   Total Questions :  
+   Total Questions :  06
 ============================================================
 
 """
